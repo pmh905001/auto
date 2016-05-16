436cbca6ef10e60c431570c93c8e20242e143edd
pmh905001 <pmh905001@126.com>
2016-05-16 23:25:24 +0800

    Update password to 02

--- login-jabber.sikuli/login-jabber.py
+++ login-jabber.sikuli/login-jabber.py
@@ -1,6 +1,6 @@
 openApp("jabber2.bat");
 wait(Pattern("1460617164836.png").similar(0.50),60)
-password="your-password"
+password="your-password-002"
 type("1460617281533.png", password)
 click("1460617323842.png")
 
