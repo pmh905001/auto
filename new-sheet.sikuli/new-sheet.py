9e997833e6f58f9f843af742e4f59204e6b81769
pmh905001 <pmh905001@126.com>
2016-05-18 22:45:05 +0800

    Make sure the excel UI on top , add comments for this feature

--- new-sheet.sikuli/new-sheet.py
+++ new-sheet.sikuli/new-sheet.py
@@ -1,15 +1,23 @@
 import time
+############################################################################
+#
+#This script help me to create new sheet, named as %month-%day formated
+#
+#############################################################################
 
-click("1463535593042.png")
+#Make excel UI on top
+if not exists("1463581342128.png"):
+    click("1463535593042.png")
 click("1463535665977.png")
 dragDrop("1463535726227.png", "1463535759063.png")
 
 ISOTIMEFORMAT="%m-%d"
-myTime=time.strftime( ISOTIMEFORMAT, time.localtime() )
-print myTime
+myDate=time.strftime( ISOTIMEFORMAT, time.localtime() )
+print myDate
 
 doubleClick("1463536447521.png")
-type(myTime)
+type(myDate)
+click("1463582110026.png")
 type("s",KEY_CTRL)
 
 
